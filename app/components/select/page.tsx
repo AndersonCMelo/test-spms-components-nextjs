80d9cd74dde6d9707631c9b5405ab9912db99f7b
Anderson Melo <anderson@stalo.digital>
2024-05-14 19:36:43 +0100

    Add new version

--- app/components/select/page.tsx
+++ app/components/select/page.tsx
@@ -2,15 +2,138 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
-// const Select = dynamic(async () => (await import('@spms/ui-react')).Select, {ssr: false})
+const Select = dynamic(async () => (await import('@spms/ui-react')).Select, {ssr: false})
 
 export default function Home() {
+  const options = [
+    { label: 'Australia', value: 'AU' },
+    { label: 'Brazil', value: 'BR' },
+    { label: 'China', value: 'CN' },
+    { label: 'Egypt', value: 'EG', icon: 'user' },
+    { label: 'France', value: 'FR', icon: 'reload' },
+    { label: 'Germany', value: 'DE', icon: 'calendar' },
+    { label: 'India', value: 'IN' },
+    { label: 'Japan', value: 'JP' },
+    { label: 'Spain', value: 'ES' },
+    { label: 'United States', value: 'US' },
+  ]
+
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-				<h2>Select</h2>
+      <div>
+        <h2 className={styles.title}>multiple</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            state="default"
+            multiple
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+            onChange={value => console.log('value', value)}
+          />
+        </div>
+
+        <h2 className={styles.title}>multiple with chips</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            state="default"
+            multiple
+            displayChips
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+          />
+        </div>
+
+        <h2 className={styles.title}>basic</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            state="default"
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+            onChange={value => console.log('value', value)}
+          />
+        </div>
+
+        <h2 className={styles.title}>disabled</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            disabled
+            state="default"
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+          />
+        </div>
+
+        <h2 className={styles.title}>read only</h2>
+        
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            icon="calendar"
+            readonly
+            state="default"
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+          />
+        </div>
+
+        <h2 className={styles.title}>success</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            state="success"
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+          />
+        </div>
+
+        <h2 className={styles.title}>error</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            state="error"
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+          />
+        </div>
+
+        <h2 className={styles.title}>info</h2>
+
+        <div className={styles['input-container']}>
+          <Select
+            label="Country"
+            options={options}
+            state="info"
+            input={{
+              placeholder: 'Selecionar país',
+            }}
+          />
+        </div>
       </div>
     </main>
   );
--- app/template.tsx
+++ app/template.tsx
@@ -3,6 +3,8 @@ import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation'
 import styles from "./template.module.css";
 
+const SPMSTemplate = dynamic(async () => (await import('@spms/ui-react')).Template, {ssr: false})
+
 const Header = dynamic(async () => (await import('@spms/ui-react')).Header, {ssr: false});
 const handleConfig = console.log.bind(null, 'HandleConfig')
 const handleLogout = console.log.bind(null, 'handleLogout')
@@ -179,59 +181,271 @@ export default function Template({
   children: React.ReactNode;
 }>){
 	const router = useRouter();
-	return ( <>
-			<main className={styles.template__main}>
-				<aside className={styles.template__menu}>
+
+	const menuOptions = {
+    opened: true,
+    link: '/',
+    state: 'index',
+    logo: {
+      fontIcon: 'spms',
+      size: '150px',
+      color: 'primary',
+    },
+    smallLogo: {
+      fontIcon: 'spms-icon',
+      size: '50px',
+      color: 'primary',
+    },
+    ticket: {
+      title: 'Está com dúvidas?',
+      titleVariant: 'Ticket',
+      url: '/',
+      state: 'ticket state',
+      icon: 'ticket-icon',
+      description: 'Crie o seu Ticket ou Pedido aqui',
+    },
+  }
+
+  const menuItems = [
+    {
+      title: 'Início',
+      icon: 'home',
+      url: '/',
+      state: 'index'
+    },
+    {
+      title: 'Administração',
+      icon: 'admin',
+      children: [
+        {
+          title: 'Criar Lista de Utentes',
+          icon: 'view-card',
+          url: '/components/Tabs',
+          state: 'Tabs',
+        },
+        {
+          title: 'Equipas',
+          icon: 'view-card',
+          children: [
+            {
+              title: 'Equipa 1',
+              icon: 'view-card',
+              url: '/',
+              state: '',
+            },
+            {
+              title: 'Equipa 2',
+              icon: 'view-card',
+              url: '/',
+              state: '',
+            },
+          ],
+        },
+        {
+          title: 'Histórico de Transferências',
+          icon: 'view-card',
+          url: '/',
+          state: '',
+        },
+      ]
+    },
+    {
+      title: 'Componentes',
+      icon: <SPMSIcon fontIcon="components" size="medium" />,
+      url: '/',
+      state: '',
+      children: [
+        {
+          title: 'Accordion',
+          icon: 'view-card',
+          url: '/components/accordion',
+          state: 'Accordion',
+        },
+        {
+          title: 'Avatar',
+          icon: 'view-card',
+          url: '/components/avatar',
+          state: 'Avatar',
+        },
+        {
+          title: 'Autocomplete',
+          icon: 'view-card',
+          url: '/components/autocomplete',
+          state: 'Autocomplete',
+        },
+        {
+          title: 'BottonNavigation',
+          icon: 'view-card',
+          url: '/components/bottom-navigation',
+          state: 'BottonNavigation',
+        },
+        {
+          title: 'Breadcrumb',
+          icon: 'view-card',
+          url: '/components/breadcrumb',
+          state: 'Breadcrumb',
+        },
+        {
+          title: 'Button',
+          icon: 'view-card',
+          url: '/components/button',
+          state: 'Button',
+        },
+        {
+          title: 'Checkbox',
+          icon: 'view-card',
+          url: '/components/checkbox',
+          state: 'Checkbox',
+        },
+        {
+          title: 'Chips',
+          icon: 'view-card',
+          url: '/components/chips',
+          state: 'Chips',
+        },
+        {
+          title: 'Divider',
+          icon: 'view-card',
+          url: '/components/divider',
+          state: 'Divider',
+        },
+        {
+          title: 'Pagination',
+          icon: 'view-card',
+          url: '/components/pagination',
+          state: 'Pagination',
+        },
+        {
+          title: 'RadioButton',
+          icon: 'view-card',
+          url: '/components/radio-button',
+          state: 'RadioButton',
+        },
+        {
+          title: 'Select',
+          icon: 'view-card',
+          url: '/components/select',
+          state: 'Select',
+        },
+        {
+          title: 'Stepper',
+          icon: 'view-card',
+          url: '/components/stepper',
+          state: 'Stepper',
+        },
+        {
+          title: 'Switch',
+          icon: 'view-card',
+          url: '/components/switch',
+          state: 'Switch',
+        },
+        {
+          title: 'Table',
+          icon: 'view-card',
+          url: '/components/table',
+          state: 'Table',
+        },
+        {
+          title: 'Tabs',
+          icon: 'view-card',
+          url: '/components/tabs',
+          state: 'Tabs',
+        },
+        {
+          title: 'TextInput',
+          icon: 'view-card',
+          url: '/components/text-input',
+          state: 'TextInput',
+        },
+        {
+          title: 'TransferList',
+          icon: 'view-card',
+          url: '/components/transfer-list',
+          state: 'TransferList',
+        },
+        {
+          title: 'Tree',
+          icon: 'view-card',
+          url: '/components/tree',
+          state: 'Tree',
+        },
+      ],
+    },
+    {
+      title: 'Identificação',
+      icon: <SPMSIcon fontIcon="identification" size="medium" />,
+      url: '/',
+      children: [
+        {
+          title: 'Test 1',
+          icon: 'view-card',
+          url: '/components/Tabs',
+          state: 'Tabs',
+        },
+      ],
+    },
+  ]
+
+  const headerOptions = {
+    title: 'Meus alertas',
+    notifications: [
+      {
+        label: 'Notificações',
+        content: [
+          { date: 'Há 1 hora', title: 'Notificação 1', icon: 'documents' },
+          { date: 'Há 1 hora', title: 'Notificação 2', icon: 'new-request' },
+        ]
+      },
+      {
+        label: 'Alertas',
+        content: [
+          { date: 'Há 1 hora', title: 'Alerta 1', icon: 'alert' },
+          { date: 'Há 1 hora', title: 'Alerta 2', icon: 'wrong-report' },
+        ]
+      },
+    ],
+    paths: {
+      openAllNotifications: '/notifications',
+      personalData: '/personal-data',
+      webAccessibility: '/acessibility'
+    }
+  }
+
+	return (
+		<>
+			<SPMSTemplate
+				menu={
 					<Menu
-						options={options} 
-						items={items}
+						options={menuOptions}
+						items={menuItems}
 						onNavigate={
-							(url: string, innerState: string) => router.push(url, { scroll: false })
+							(url: string, _: string) => router.push(url)
 						}
 					/>
-				</aside>
-				<section className={styles.template__content}>
+				}
+				header={
 					<Header
 						variant="simple"
 						username="Nuno Oliveira"
 						profilePicture="https://randomuser.me/api/portraits/men/34.jpg"
 						projectName="Equipe PACE"
+						primaryActionIcon="search"
+						secondaryActionIcon="definitions"
 						notification={5}
-						options={{
-							title: 'Meus alertas',
-							notifications: [
-								{
-									label: 'Notificações',
-									content: [
-										{ date: 'Há 1 hora', title: 'Notificação 1', icon: 'documents' },
-										{ date: 'Há 1 hora', title: 'Notificação 2', icon: 'new-requests' },
-									]
-								},
-								{
-									label: 'Alertas',
-									content: [
-										{ date: 'Há 1 hora', title: 'Alerta 1', icon: 'alert' },
-										{ date: 'Há 1 hora', title: 'Alerta 2', icon: 'information' },
-									]
-								},
-							],
-							paths: {
-								openAllNotifications: '/notifications',
-								personalData: '/personal-data',
-								webAccessibility: '/acessibility'
-							}
-						}}
-						configClick={() => handleConfig()}
-						logoutClick={() => handleLogout()}
+						options={headerOptions}
+						configClick={() => {}}
+						logoutClick={() => {}}
 					/>
-					<section className={styles.template__content_wrapper}>
+				}
+				content={
+					<section className={styles.template__content}>
 						{children}
 					</section>
-				</section>
-				<footer className={styles.template__footer}>
+				}
+				footer={
 					<Footer
 						footerText="Powered by SPMS"
-						spmsLogo={false}
+						spmsLogo={true}
 						copyrightText="Todos os direitos reservados ao Governo da República Portuguesa - Ministério da Saúde"
 						logos={[
 							{
@@ -251,8 +465,8 @@ export default function Template({
 							},
 						]}
 					/>
-				</footer>
-			</main>
+				}
+			/>
 		</>
 	)
 }
