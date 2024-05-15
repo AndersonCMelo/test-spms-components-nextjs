0fd9203b7c4918a7d242df236fe413b811b0329c
Anderson Melo <anderson@stalo.digital>
2024-05-15 16:25:19 +0100

    Fix the next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.optimization = {
-			splitChunks:{
-				name: false,
-				chunks: `all`,
-				cacheGroups: {
-					vendor: {
-						chunks: 'all',
-						reuseExistingChunk: true,
-						test: /[\\/]node_modules[\\/]/,
-						minSize: 0,
-						minChunks: 1,
-						maxAsyncRequests: 300,
-						maxInitialRequests: 300,
-						name: 'vendor'
-					},
-					default: false,
-					defaultVendors: false
-				}
-			}
-		}
-    return config
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
